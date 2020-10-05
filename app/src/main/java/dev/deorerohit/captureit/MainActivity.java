baaacf15b1d624e0644738505ce01f5220f3c7bc
Rohit Deore <deorerohit18@gmail.com>
2020-10-05 18:50:26 +0530

    minor changes

--- app/src/main/java/dev/deorerohit/captureit/MainActivity.java
+++ app/src/main/java/dev/deorerohit/captureit/MainActivity.java
@@ -1,29 +1,50 @@
 package dev.deorerohit.captureit;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
-
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+
+import static android.os.Environment.DIRECTORY_PICTURES;
+import static android.os.Environment.getExternalStoragePublicDirectory;
 
 public class MainActivity extends AppCompatActivity {
 
     FloatingActionButton floatingActionButton;
     ImageView imageView;
+    GridView gridView;
+
     private String currentPhotoPath;
+    ArrayList<String> allImages;
+
+    ImageGridViewAdapter imageGridViewAdapter;
+
+
+    private static final int MY_READ_PERMISSION_CODE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,33 +52,68 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         floatingActionButton = findViewById(R.id.add_floating_btn);
-        imageView = findViewById(R.id.imageView);
+        gridView = findViewById(R.id.gridview);
+
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_READ_PERMISSION_CODE);
+        } else {
+            //  loadImages();
+        }
+
+
+// /storage/emulated/0/Android/data/dev.deorerohit.captureit/files/Pictures/androidProjectsPhotos6478409151517135181.jpg
+
+        File file = getExternalStoragePublicDirectory(DIRECTORY_PICTURES);
+        final Context context = MainActivity.this;
 
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String fileName = "androidProjectsPhotos";
 
-                File storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                //   File storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                File storageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+
 
                 try {
                     File imageFile = File.createTempFile(fileName, ".jpg", storageDirectory);
+
                     currentPhotoPath = imageFile.getAbsolutePath();
 
                     Uri imageUri = FileProvider.getUriForFile(MainActivity.this, "dev.deorerohit.captureit.fileprovider", imageFile);
-
                     Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                     intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
                     startActivityForResult(intent, 1);
-                } catch (IOException e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
-
-
             }
         });
     }
 
+    private void loadImages() {
+        allImages = GetImages.listOfImages(this);
+        imageGridViewAdapter = new ImageGridViewAdapter(this, allImages);
+        gridView.setAdapter(imageGridViewAdapter);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == MY_READ_PERMISSION_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                //  loadImages();
+            } else {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -66,7 +122,10 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == 1 && resultCode == RESULT_OK) {
 
             Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath);
+
             imageView.setImageBitmap(bitmap);
+            System.out.println(bitmap);
+            //  ImageGridViewAdapter.bitmapArrayList.add(bitmap);
 
         }
     }
